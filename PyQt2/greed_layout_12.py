8cf39f4cdc7a623cf383538edaaa4d2a28b78793
aly <y307@ukr.net>
2018-03-26 11:01:42 +0300

    20180326@work

---
+++ PyQt2/greed_layout_12.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# http://python-3.ru/page/layout-management-in-pyqt5
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QGridLayout, QPushButton, QApplication, QStyleFactory)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        names = ['Cls', 'Bck', '', 'Close', '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=',
+                 '+']
+        positions = [(i, j) for i in range(5) for j in range(4)]
+
+        for position, name in zip(positions, names):
+            if name == '':
+                continue
+            button = QPushButton(name)
+            grid.addWidget(button, *position)
+
+        self.move(300, 150)
+        self.setWindowTitle('Calculator')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+print(QStyleFactory.keys())
--- PyQt2/menu_toolbat_statusbar_09.py
+++ PyQt2/menu_toolbat_statusbar_09.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # http://python-3.ru/page/menu-toolbar-pyqt-python
 
