b3e4777f1c3f4479adc363bba6c8553614213264
aly <y307@ukr.net>
2018-03-27 00:44:45 +0300

    20180326@home

--- PyQt5_2/24_QSlider.py
+++ PyQt5_2/24_QSlider.py
@@ -12,6 +12,7 @@ class Example(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.label = QLabel(self)
         self.initUI()
 
     def initUI(self):
@@ -20,7 +21,6 @@ class Example(QWidget):
         sld.setGeometry(30, 40, 100, 30)
         sld.valueChanged[int].connect(self.changeValue)
 
-        self.label = QLabel(self)
         self.label.setPixmap(QPixmap('mute.png'))
         self.label.setGeometry(160, 40, 80, 30)
 
@@ -31,9 +31,9 @@ class Example(QWidget):
     def changeValue(self, value):
         if value == 0:
             self.label.setPixmap(QPixmap('mute.png'))
-        elif value > 0 and value <= 30:
+        elif (value > 0) and (value <= 30):
             self.label.setPixmap(QPixmap('min.png'))
-        elif value > 30 and value < 80:
+        elif (value > 30) and (value < 80):
             self.label.setPixmap(QPixmap('med.png'))
         else:
             self.label.setPixmap(QPixmap('max.png'))
