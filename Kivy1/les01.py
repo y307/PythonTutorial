eeb4331dfbbef1c5fbce9164e73fab1a2f3bea80
aly <y307@ukr.net>
2018-04-02 15:54:35 +0300

    20180102@work

--- Kivy1/les01.py
+++ Kivy1/les01.py
@@ -9,7 +9,8 @@ class MyApp(App):
 
     def build(self):
         return Button(text='Это кнопка',
-                      font=30,
+                      background_color=(0, 0, 1, 1),
+                      font_size=30,
                       on_press=self.btn_press)
 
     def btn_press(self, instance):
